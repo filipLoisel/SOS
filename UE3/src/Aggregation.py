69ff4e0e5a15eaef89283320d53b2d5478f8a573
Frodl <flohue3@hotmail.com>
2022-01-29 13:24:19 +0100

    push

--- UE3/src/Aggregation.py
+++ UE3/src/Aggregation.py
@@ -3,81 +3,84 @@ from numpy.random import randint
 from scipy.cluster.hierarchy import ward, fcluster
 
 
-def create_lines(n_rows = 5, n_columns = 6):
-    lines = []
-    for i in range(n_rows):
-        lines.append([(randint(0,10),randint(0,10)) for _ in range(n_columns)])
-    return np.array(lines)
-
-
-def distance(l1,l2):
+def distance(l1, l2):
     "l1 and l2 need to be 2 dimensional numpy arrays"
     distance_normal = 0
     distance_reversed = 0
     for i in range(len(l1)):
         distance_normal += np.linalg.norm(l1[i] - l2[i])
-        distance_reversed += np.linalg.norm(l1[i] - l2[-(i+1)])
+        distance_reversed += np.linalg.norm(l1[i] - l2[-(i + 1)])
     l = [distance_normal, distance_reversed]
-    return min(l),np.where(l == min(l))[0][0] # [0][0] to get an int instead of an array
+    return min(l), np.where(l == min(l))[0][0]  # [0][0] to get an int instead of an array
 
 
 def create_distance_matrix(lines):
     # create mattrix that shows if the pairwise distance is revesed or not
     n_lines = lines.shape[0]
-    matrix = np.empty((n_lines,n_lines))
-    normal_or_not = np.empty((n_lines,n_lines))
+    matrix = np.empty((n_lines, n_lines))
+    normal_or_not = np.empty((n_lines, n_lines))
     for i in range(n_lines):
         for j in range(n_lines):
             if i == j:
-                matrix[i,j] = np.inf
-                normal_or_not[i,j] = -1
+                matrix[i, j] = np.inf
+                normal_or_not[i, j] = -1
             else:
-                matrix[i,j] = distance(lines[i],lines[j])[0]
-                normal_or_not[i,j] = distance(lines[i],lines[j])[1]
+                matrix[i, j] = distance(lines[i], lines[j])[0]
+                normal_or_not[i, j] = distance(lines[i], lines[j])[1]
 
-    return np.triu(matrix,1), normal_or_not
+    return np.triu(matrix, 1), normal_or_not
 
 
-def assign_clusters(d_matrix,threshold,print_Z=False):
+def assign_clusters(d_matrix, threshold, print_Z=False):
     Z = ward(d_matrix[d_matrix > 0])
     # see link for explanation of Z
     # https://docs.scipy.org/doc/scipy/reference/reference/generated/scipy.cluster.hierarchy.fcluster.html#scipy.cluster.hierarchy.fcluster
     if print_Z:
         print(Z)
     assigned_clusters = fcluster(Z, threshold, criterion='distance')
-    print(assigned_clusters)
     return assigned_clusters
 
-def average_lines(lines,clusters,r_matrix):
+
+def average_lines(lines, clusters, r_matrix, names):
     ''' averages x,y values of all lines per cluster
     '''
-    #final_lines = np.empty((clusters.max(),lines[],2))
+
+    reverse_list = []
     final_lines = []
-    for i in range(max(clusters)): #iterate trough clusters
+    final_names = []
+    for i in range(max(clusters)):  # iterate trough clusters
+        reverse_used = False #indicates if the cluster contains a reversed line
         cluster_lines = np.where(clusters == i + 1)
         if len(cluster_lines[0]) == 1:
             final_lines.append(lines[cluster_lines[0]][0].tolist())
+            final_names.append(names[cluster_lines[0][0]])
+            reverse_list.append(reverse_used)
             continue
 
-
         first_ind = cluster_lines[0][0]
+        name = names[first_ind]
         averaged_line = lines[first_ind]
-        for compare_ind in cluster_lines[0]:   #itearate trough lines
+        for compare_ind in cluster_lines[0]:  # itearate trough lines
             if r_matrix[first_ind, compare_ind] == 0:  # normal distance
                 averaged_line = averaged_line + lines[compare_ind]
+                name += " + " + names[compare_ind]
 
             elif r_matrix[first_ind, compare_ind] == 1:  # reverse distance
-                averaged_line = averaged_line + np.flip(lines[compare_ind],axis=0)
+                name += " + " + names[compare_ind]
+                reverse_used = True
+                averaged_line = averaged_line + np.flip(lines[compare_ind], axis=0)
         averaged_line = (averaged_line / len(cluster_lines[0])).tolist()
-
+        reverse_list.append(reverse_used)
         final_lines.append(averaged_line)
-    return final_lines
+        final_names.append(name)
+
+    return final_lines, final_names, reverse_list
 
 
-def aggregate(lines,threshold):
+def aggregate(lines, threshold, names, print_Z=False):
     d_matrix, r_matrix = create_distance_matrix(lines)
-    clusters = assign_clusters(d_matrix, threshold)
-    new_lines = average_lines(lines, clusters, r_matrix)
+    clusters = assign_clusters(d_matrix, threshold, print_Z)
+    new_lines = average_lines(lines, clusters, r_matrix, names)
     new_lines = np.array(new_lines)
     return new_lines
 
@@ -85,6 +88,7 @@ def aggregate(lines,threshold):
 
 
 if __name__ == '__main__':
+    names = ['a','b','c']
     #lines = create_lines()
     lines = [[[ 3.66101695,  6.23728814],
   [ 6.18181818,  9.03030303],
@@ -119,10 +123,11 @@ if __name__ == '__main__':
   [ 9.61363636, 12.25],
   [10.48387097,  6.14516129]]]
     lines = np.array(lines)
-    new_lines = aggregate(lines, 100)
+    new_lines,names, reverse_list = aggregate(lines, 80 ,names)
+    print(names,reverse_list)
    # print("Original lines {} \nAggregated lines {}".format(lines,new_lines))
-    print(new_lines)
-    print(np.array(new_lines).shape)
+    #print(new_lines)
+    #print(np.array(new_lines).shape)
 
 
 
